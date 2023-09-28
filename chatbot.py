3ab13965f86249ab436046cb78e0794138f2968d
VARAD GANJOO <102558611@student.swin.edu.au>
2023-09-29 00:42:22 +1000

    Json data storage and json access

--- chatbot.py
+++ chatbot.py
@@ -1,15 +1,15 @@
 import os
 import openai
+import json
 import time
 
 # Set your API key as an environment variable or load it from a file
-os.environ["OPENAI_API_KEY"] = "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"
+os.environ["OPENAI_API_KEY"] = "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"
 
 # Authenticate with OpenAI API
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
 def get_response(prompt, user_input):
-    # Set up API request parameters
     parameters = {
         "engine": "text-davinci-002",
         "prompt": f"{prompt}\nVictim: {user_input}\nAttacker: ",
@@ -18,71 +18,43 @@ def get_response(prompt, user_input):
         "stop": None,
         "temperature": 0.5,
     }
-
-    # Use OpenAI API to generate response
     response = openai.Completion.create(**parameters)
+    return response.choices[0].text.strip()
 
-    # Extract response text from API response
-    response_text = response.choices[0].text.strip()
+def get_preference(aspects, pref_type):
+    prefs = aspects[pref_type]
+    print(f"Choose a {pref_type}:")
+    for i, pref in enumerate(prefs, 1):
+        print(f"{i}. {pref['name']}")
+    choice = int(input("Enter the number corresponding to your choice: "))
+    return prefs[choice - 1]['description']
 
-    return response_text
+def generate_super_prompt(aspects):
+    topics = get_preference(aspects, "topics")
+    conditions = get_preference(aspects, "conditions")
+    flow = get_preference(aspects, "conversation_flow")
+    background = get_preference(aspects, "backgrounds")
+    evidence = get_preference(aspects, "evidence")
+    behaviors = get_preference(aspects, "behaviors")
+    incidents = get_preference(aspects, "incidents")
+    actors = get_preference(aspects, "actors")
+    threats = get_preference(aspects, "threats")
+    victim = get_preference(aspects, "victim")
 
-def generate_backstory(attacker_type, attack_type, victim_name):
-    # Generate backstory using GPT-3
-    prompt = f"Generate a backstory for a {attacker_type} cyber attacker who used a {attack_type} attack against {victim_name}. Use first person language and pretend you are speaking directly to me about your backstory"
-    response = openai.Completion.create(
-        engine="text-davinci-002",
-        prompt=prompt,
-        max_tokens=1024,
-        n=1,
-        stop=None,
-        temperature=0.5,
-    )
-    backstory = response.choices[0].text.strip()
+    super_prompt = f"You are {actors} who used a {threats} against {victim}. {background} {incidents} {behaviors} {evidence} {flow} {conditions} \nAttacker: "
+    return super_prompt
 
-    # Generate demands using GPT-3
-    prompt = f"Generate demands for the {attacker_type} cyber attacker who used a {attack_type} attack against {victim_name}. Use first person language and pretend you are speaking directly to me about your demands"
-    response = openai.Completion.create(
-        engine="text-davinci-002",
-        prompt=prompt,
-        max_tokens=1024,
-        n=1,
-        stop=None,
-        temperature=0.5,
-    )
-    demands = response.choices[0].text.strip()
+# Load aspects from the consolidated JSON file
+with open("aspects.json", "r") as f:
+    aspects = json.load(f)
 
-    # Generate initial prompt
-    prompt = f"You are a {attacker_type} cyber attacker who used a {attack_type} attack against my company, {victim_name}. {backstory} {demands}. You are speaking to me through Whats App. \nAttacker: "
+# Generate super prompt using the provided preferences
+prompt = generate_super_prompt(aspects)
 
-    return prompt
-
-# Prompt user for information about the cyber attack
-attacker_type = input("What type of cyber attacker are you? ")
-attack_type = input("What cyber attack did you use against your victim? ")
-victim_name = input("Who is your victim? ")
-
-# Generate initial prompt using the provided information
-prompt = generate_backstory(attacker_type, attack_type, victim_name)
-
-# Generate response from model without context
-response = get_response(prompt, "")
-
-# Print attacker's initial message
-print(f"Attacker: {response}")
-
+# Chat loop
 while True:
-    # Prompt user for input
     user_input = input("Victim: ")
-
-    # Generate response from model with context
     response = get_response(prompt, user_input)
-
-    # Print response
     print(f"Attacker: {response}")
-
-    # Wait for a second
     time.sleep(1)
-
-    # Update the prompt with the new response
     prompt += f"Victim: {user_input}\nAttacker: {response}"
