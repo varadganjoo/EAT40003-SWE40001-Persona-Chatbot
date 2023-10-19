2e43c469be2bb4fe651dbf7ffc2c2381eccedce5
VARAD GANJOO <102558611@student.swin.edu.au>
2023-10-19 11:02:50 +1100

    Improve code readability and efficiency

--- chatbot.py
+++ chatbot.py
@@ -4,6 +4,18 @@ import openai
 def read_content_from_file(filename="content.txt"):
     with open(filename, 'r') as file:
         return file.read()
+ 
+def call_GPT(messages):
+    #AI values can be tuned for accuracy
+    return openai.ChatCompletion.create(
+        model="gpt-3.5-turbo-16k",
+        messages=messages,
+        temperature=0.5,
+        max_tokens=256,
+        top_p=1,
+        frequency_penalty=0.22,
+        presence_penalty=0.15
+    )
 
 def openai_chatbot():
     # Set OpenAI API key
@@ -15,15 +27,8 @@ def openai_chatbot():
     messages = [{"role": "system", "content": content}]
     
     # Have the model initiate the conversation
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo-16k",
-        messages=messages,
-        temperature=0.5,
-        max_tokens=256,
-        top_p=1,
-        frequency_penalty=0.22,
-        presence_penalty=0.15
-    )
+    response = call_GPT(messages)
+    
     assistant_response = response['choices'][0]['message']['content']
     messages.append({"role": "assistant", "content": assistant_response})
     print(f"Attacker: {assistant_response}")
@@ -34,15 +39,7 @@ def openai_chatbot():
         messages.append({"role": "user", "content": user_input})
         
         # Getting model's response
-        response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo-16k",
-            messages=messages,
-            temperature=0.5,
-            max_tokens=256,
-            top_p=1,
-            frequency_penalty=0.22,
-            presence_penalty=0.15
-        )
+        response = call_GPT(messages)
         assistant_response = response['choices'][0]['message']['content']
         messages.append({"role": "assistant", "content": assistant_response})
         
